a8f2aeb998cdecc23da195b2d8ed00d46e71fcbd
Prodigy-Genes <bulletoze@gmail.com>
2023-07-14 21:01:11 +0000

    Made some modifcations for the HeaderButtons

--- Components/HeaderButtons.tsx
+++ Components/HeaderButtons.tsx
@@ -3,43 +3,45 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-
 export const HeaderButtons = () => {
   return (
-    <View style={styles.container}>
+    <View style={styles.headerContainer}>
       <View style={styles.row}>
         <Text style={styles.Text}>WhatsApp</Text>
+        
+        <View style={styles.iconsContainer}>
+          <TouchableOpacity>
+            <Ionicons name="ios-camera" size={24} color="grey" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="search" size={24} color="gray" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <MaterialIcons name="more-vert" size={24} color="grey" />
+          </TouchableOpacity>
+        </View>
       </View>
 
-      <TouchableOpacity style={styles.under}>
+      <View style={styles.tabsContainer}>
+
+      <TouchableOpacity>
         <MaterialIcons name="group" size={20} color="grey" />
       </TouchableOpacity>
-
-      <View style={styles.icons}>
+        
         <TouchableOpacity>
-          <Ionicons name="ios-camera" size={20} color="grey" />
+          <View>
+            <Text style={styles.crewText}>Chats</Text>
+            <View style={styles.circle} />
+             <Text style={styles.circleText}>18</Text>
+          </View>
+          
         </TouchableOpacity>
         <TouchableOpacity>
-          <Ionicons name="search" size={20} color="gray" />
-        </TouchableOpacity>
+          <Text style={styles.crewText}>Status </Text>
+          <View style={styles.smalldot} /> 
+        </TouchableOpacity> 
         <TouchableOpacity>
-          <MaterialIcons name="more-vert" size={20} color="grey" />
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.crew}>
-        <View style={styles.circle}>
-          <Text style={styles.circleText}>18</Text>
-        </View>
-        <View style={styles.smalldot} />
-        <TouchableOpacity>
-          <Text style={styles.crewText}>Chats</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.crewText}>Status</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.crewText}>Calls</Text>
+          <Text style={styles.crewText}>Calls </Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -47,65 +49,52 @@ export const HeaderButtons = () => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 16,
-    paddingVertical: 30,
+  headerContainer: {
+    height: 170,
+    paddingTop: 60,
     backgroundColor: "#07201d",
     borderBottomWidth: 1,
-    alignItems: "flex-start", // Align items to the start of the container
+    justifyContent: "space-around"
+  },
+  smalldot: {
+    backgroundColor: "grey",
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 9,
+    left: 37,
+    
+  },
+  row: {
     flexDirection: "row",
-    justifyContent: "space-between"
+    alignItems: "center",
+    width: '100%',
+    justifyContent: "space-between",
     
   },
 
-  smalldot: {
-    width: 8,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: "grey",
-    bottom: 10,
-    right: -28,
-    position: "absolute"
-  },
-
-  row: {
-    flexDirection: "row",
-    alignItems: "center", // Add alignItems property to vertically align the text
-    marginBottom: 20,
-  },
-
-  under: {
-    position: "absolute", // Position the group icon absolutely
-    bottom: 15, // Align the group icon to the bottom
-    left: 0, // Align the group icon to the left
-    marginLeft: 16, // Add some left margin to the group icon
-  },
-  icons: {
+  iconsContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
     marginRight: 10,
     width: 100,
-    marginBottom: 20
   },
-  crew: {
+  tabsContainer: {
     flexDirection: "row",
-    alignItems: "center", // Add alignItems property to vertically align the crew text
-    position: "absolute",
-    bottom: 15,
-    left: 0,
-    marginLeft: 120,
+    alignItems: "center",
     justifyContent: "space-around",
-    width: 100,
-    
+    width: '100%',
+    left: -20
   },
   crewText: {
     color: "grey",
     fontSize: 16,
-    marginLeft: 80,
-    
-    
+   
+    marginRight: 8, // Add marginRight for space between crewText items
   },
-
   circle: {
     backgroundColor: "gray",
     width: 20,
@@ -114,19 +103,22 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     position: "absolute",
-    top: -10,
-    left: 0
-    
+    bottom: 10,
+    left: 30,
   },
   circleText: {
     color: "black",
-    fontSize: 10,
+    fontSize: 15,
     fontWeight: "bold",
+    position: "absolute",
+    bottom: 14,
+    left: 32,
+   
   },
   Text: {
     color: "gray",
-    fontSize: 18,
+    fontSize: 24,
     fontWeight: 'bold',
+    left: 15
   },
 });
-
